6bd2c34eade90118a4d893e9a4a2c67e177ae002
hung <buihungtpd3@gmail.com>
2026-05-29 23:57:27 +0900

    refactor: remove SynBrain and commands.ipynb from tracking

--- SynBrain/src/physio/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .kuramoto import KuramotoODE, KuramotoLayer
-from .hemo import BalloonWindkessel
-from .hybrid_model import PhysicalDecoderVAE, ResidualPhysicsVAE
-from .prior_sampler import BioLatentSampler
